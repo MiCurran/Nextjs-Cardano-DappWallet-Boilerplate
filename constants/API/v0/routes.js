446d565c97c5b0c094d8b1fa74f85492651d224e
Michael Curran <mcurran1722@gmail.com>
2021-10-18 10:16:17 -0600

    adjust base API according to env

--- constants/API/v0/routes.js
+++ constants/API/v0/routes.js
@@ -1,4 +1,4 @@
-const baseApi = 'http://localhost:3000';
+const baseApi = process.env.BASE_API;
 
 export const LATEST_BLOCK = `${baseApi}/api/v0/cardano/latest-block`;
 export const ADDRESSES = `${baseApi}/api/v0/cardano/addresses`;
